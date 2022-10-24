e79ecfdf179f0e7c2909426e8eebaa3d3e4db437
Jevgeņijs Bubins <103423233+22DP1JBubi@users.noreply.github.com>
2022-10-24 16:34:51 +0300

    Add files via upload

---
+++ tkinter_gui_1.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+
+def bAaction():
+    print('Thank you!')
+
+
+def bBaction():
+    print('Ouch! That hurt!')
+
+
+window = Tk()
+buttonA = Button(window, text='Press me!', command=bAaction)
+buttonB = Button(window, text='Don\'t press!', command=bBaction)
+buttonA.pack()
+buttonB.pack()
